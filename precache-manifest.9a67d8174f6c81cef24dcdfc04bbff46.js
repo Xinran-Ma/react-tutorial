4804e7da7164466af0ad9b7aa931cba1bbc2dc65
Ray Ma <mxr891029@hotmail.com>
2019-04-13 23:36:01 +1000

    Updates

---
+++ precache-manifest.9a67d8174f6c81cef24dcdfc04bbff46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bcee853330bb479613d",
+    "url": "/Xinran-Ma/react-tutorial/static/css/main.01b3d2f7.chunk.css"
+  },
+  {
+    "revision": "5bcee853330bb479613d",
+    "url": "/Xinran-Ma/react-tutorial/static/js/main.0924986f.chunk.js"
+  },
+  {
+    "revision": "d2f2df0198501ca8871f",
+    "url": "/Xinran-Ma/react-tutorial/static/js/runtime~main.d06e21af.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/Xinran-Ma/react-tutorial/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "7b901670bb2d61f73cf6d78130eb25a4",
+    "url": "/Xinran-Ma/react-tutorial/index.html"
+  }
+];
